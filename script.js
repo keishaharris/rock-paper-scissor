78c55cfccc67b4d5939aece914d5e79c69fbed74
Keisha Harris <keishadharriss@gmail.com>
2022-04-14 10:27:55 -0400

    Finalized all functions for 5 round game

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 const PLAY_OPTIONS = ['rock', 'scissor', 'paper']
 
 let computerSelection;
-let playerSelection = 'scissor';
+let playerSelection;
+
+
+
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay(){
     return PLAY_OPTIONS[Math.floor(Math.random() * PLAY_OPTIONS.length)];
@@ -19,39 +24,46 @@ function playRound(playerSelection, computerSelection, PLAY_OPTIONS){
         playerSelection === PLAY_OPTIONS[1] && computerSelection === PLAY_OPTIONS[2] ||
         playerSelection === PLAY_OPTIONS[2] && computerSelection === PLAY_OPTIONS[0]
         ){
-        return 'Player';
+        ++playerScore;
+        console.log('Player', playerScore)
+        return playerScore;
         }
     else if (
         computerSelection === PLAY_OPTIONS[0] && playerSelection === PLAY_OPTIONS[1] ||
         computerSelection === PLAY_OPTIONS[1] && playerSelection === PLAY_OPTIONS[2] ||
         computerSelection === PLAY_OPTIONS[2] && playerSelection === PLAY_OPTIONS[0]
         ){
-        return 'Computer';
+        ++computerScore;
+        console.log('Computer', computerScore)
+        return computerScore;
         }
     else {
         return 'Tie';
     }
 }   
+
+function checkWinner(computerScore, playerScore){
+    if (computerScore >= 1 || playerScore >= 1){
+        playerScore > computerScore ? console.log('Player Wins') : console.log('Computer Wins')
+    }
+    else{
+        console.log('Tie!')
+    }
+}
 // 5 rounds will occur and the player w/the most wins is winner
 
 function playGame(){
-    let winner = []
     let winnerRound;
     
     for(let i = 0; i < 5; i++){
-        // Winner will be noted and stored for each round
-    
         let computerSelection = computerPlay();
+        let playerSelection = window.prompt('rock, scissors or paper?').toLowerCase();;
         console.log(computerSelection)
-        let winnerRound = playRound(playerSelection, computerSelection, PLAY_OPTIONS);
-        winner.push(winnerRound);
-        console.log(winner);
+        playRound(playerSelection, computerSelection, PLAY_OPTIONS);
+        if (i == 4){
+            checkWinner(computerScore, playerScore);
+        }
     }
 }
 
 playGame();
-
-// play five rounds
-// for each round return winner 
-// take winner and store into array 
-// after 
