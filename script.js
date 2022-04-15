b1eeaebfe62d700a1f56532e54f26cf4d2ec1d2d
Keisha Harris <keishadharriss@gmail.com>
2022-04-15 13:23:35 -0400

    Added UI Styling and Functionality

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-// The user is prompted to input a selection of Rock, Paper, or Scissor
 // The computer will generate a selection from an array of options 
 const PLAY_OPTIONS = ['rock', 'scissor', 'paper']
+
+let winnerTexts = [
+    'Player wins this round!',
+    'Computer wins this round!',
+    'Tie!',
+    'Player wins the game!',
+    'You lose! Computer wins the game!'
+]
 
 let computerSelection;
 let playerSelection;
 
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissor = document.querySelector('#scissor');
+const player = document.querySelector('.player');
+const computer = document.querySelector('.computer');
+const winner = document.querySelector('.winner.score');
+const resetbtn = document.querySelector('#resetbtn');
 
+rock.addEventListener('click', (e) => {
+    playerSelection = PLAY_OPTIONS[0];
+    playGame();
+});
+paper.addEventListener('click', (e) => {
+    playerSelection = PLAY_OPTIONS[1];
+    playGame();
+});
+scissor.addEventListener('click', (e) => {
+    playerSelection = PLAY_OPTIONS[2];
+    playGame();
+});
+resetbtn.addEventListener('click', (e) => {reset();});
 
 let playerScore = 0;
 let computerScore = 0;
 
-function computerPlay(){
+player.textContent = `${playerScore}`;
+computer.textContent = `${computerScore}`;
+
+const computerPlay = () => {
     return PLAY_OPTIONS[Math.floor(Math.random() * PLAY_OPTIONS.length)];
 }
-// Wrie a function that determines users selection: 
-    // Rock > Scissor
-    // Scissor > Paper
-    // Paper > Rock 
 
-function playRound(playerSelection, computerSelection, PLAY_OPTIONS){
+const playRound = (playerSelection, computerSelection, PLAY_OPTIONS) => {
     if (
         playerSelection === PLAY_OPTIONS[0] && computerSelection === PLAY_OPTIONS[1] ||
         playerSelection === PLAY_OPTIONS[1] && computerSelection === PLAY_OPTIONS[2] ||
         playerSelection === PLAY_OPTIONS[2] && computerSelection === PLAY_OPTIONS[0]
         ){
         ++playerScore;
-        console.log('Player', playerScore)
+        player.textContent = `${playerScore}`;
+        winner.textContent = `${winnerTexts[0]}`
         return playerScore;
         }
     else if (
@@ -34,36 +61,43 @@ function playRound(playerSelection, computerSelection, PLAY_OPTIONS){
         computerSelection === PLAY_OPTIONS[2] && playerSelection === PLAY_OPTIONS[0]
         ){
         ++computerScore;
-        console.log('Computer', computerScore)
+        computer.textContent = `${computerScore}`;
+        winner.textContent = `${winnerTexts[1]}`
         return computerScore;
         }
     else {
+        winner.textContent = `${winnerTexts[2]}`
         return 'Tie';
     }
 }   
 
-function checkWinner(computerScore, playerScore){
-    if (computerScore >= 1 || playerScore >= 1){
-        playerScore > computerScore ? console.log('Player Wins') : console.log('Computer Wins')
+const reset = () => {
+    computerSelection, playerSelection;
+    computer.textContent = computerScore = 0;
+    player.textContent = playerScore = 0;
+    winner.textContent = '';
+    
+}
+
+const checkWinner = (computerScore, playerScore) => {
+    if (computerScore == 5  || playerScore == 5){
+        playerScore > computerScore ? 
+        winner.textContent = `${winnerTexts[3]}` : 
+        winner.textContent = `${winnerTexts[4]}`
     }
     else{
-        console.log('Tie!')
+        return;
     }
 }
-// 5 rounds will occur and the player w/the most wins is winner
+// 5 rounds will occur and the player w/the most wins --> winner
 
-function playGame(){
-    let winnerRound;
-    
-    for(let i = 0; i < 5; i++){
+const playGame = () =>{
+
         let computerSelection = computerPlay();
-        let playerSelection = window.prompt('rock, scissors or paper?').toLowerCase();;
-        console.log(computerSelection)
+        console.log('Player', playerSelection);
+        console.log('Computer', computerSelection);
         playRound(playerSelection, computerSelection, PLAY_OPTIONS);
-        if (i == 4){
-            checkWinner(computerScore, playerScore);
-        }
-    }
+        checkWinner(computerScore, playerScore);
 }
 
-playGame();
+
